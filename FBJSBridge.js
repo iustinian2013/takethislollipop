4cacaa41fb9e112a0d3ac72652a98469a871bc03
iustinian2013 <141711525+iustinian2013@users.noreply.github.com>
2024-06-08 21:19:56 +0300

    Update FBJSBridge.js

--- FBJSBridge.js
+++ FBJSBridge.js
@@ -169,14 +169,6 @@ function updateSwfSession(session, extendedPermissions) {
 
 }
 /*
-     FILE ARCHIVED ON 11:46:13 Nov 16, 2011 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 15:53:35 May 19, 2024.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
 playback timings (ms):
   captures_list: 9.484
   exclusion.robots: 0.103
